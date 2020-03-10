6a570fefde32a574522ddbf95000c3b03abe7b72
wmc2tokyo <wmcooper2@yahoo.com>
2020-03-10 09:40:53 +0900

    Save before branching

--- src/components/tooldata.js
+++ src/components/tooldata.js
@@ -3,7 +3,8 @@ const tools = {
   items: [
     { name: "Sentences", url: "https://teflassistant.com/tea-sentences/" },
     { name: "Dictionary", url: "https://teflassistant.com/tea-dictionary/" },
-    { name: "Volunteers", url: "https://teflassistant.com/choose-a-student/" }
+    { name: "Volunteers", url: "https://teflassistant.com/choose-a-student/" },
+    { name: "Pokemon Pronouns", url: "https://teflassistant.com/pokemon-pronouns/" }
   ]
 };
 
